15d7ab8cb5dcabd7b4f2a645096076f01b5d1825
nbaishev <nbaishev@gmail.com>
2025-05-09 16:57:06 +0600

    SensorData model changed

--- app/models.py
+++ app/models.py
@@ -24,21 +24,21 @@ class SensorData(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     created: Mapped[datetime.datetime] = mapped_column(server_default=text("CURRENT_TIMESTAMP"))
     device_id: Mapped[int] = mapped_column(unique=True)
-    water_flow: Mapped[float]
-    wind_speed: Mapped[float]
-    wind_orientation: Mapped[float]
-    sun_insolation: Mapped[float]
-    soil_temp: Mapped[float]
-    soil_hum: Mapped[float]
-    air_temp: Mapped[float]
-    air_hum: Mapped[float]
-    air_pressure: Mapped[float]
-    inside_air_temp: Mapped[float]
-    inside_air_hum: Mapped[float]
-    temp1: Mapped[float]
-    temp2: Mapped[float]
-    temp3: Mapped[float]
-    soil_temp1: Mapped[float]
-    soil_hum1: Mapped[float]
-    soil_temp2: Mapped[float]
-    soil_hum2: Mapped[float]
+    water_flow: Mapped[float] = mapped_column(nullable=True)
+    wind_speed: Mapped[float] = mapped_column(nullable=True)
+    wind_orientation: Mapped[float] = mapped_column(nullable=True)
+    sun_insolation: Mapped[float] = mapped_column(nullable=True)
+    soil_temp: Mapped[float] = mapped_column(nullable=True)
+    soil_hum: Mapped[float] = mapped_column(nullable=True)
+    air_temp: Mapped[float] = mapped_column(nullable=True)
+    air_hum: Mapped[float] = mapped_column(nullable=True)
+    air_pressure: Mapped[float] = mapped_column(nullable=True)
+    inside_air_temp: Mapped[float] = mapped_column(nullable=True)
+    inside_air_hum: Mapped[float] = mapped_column(nullable=True)
+    temp1: Mapped[float] = mapped_column(nullable=True)
+    temp2: Mapped[float] = mapped_column(nullable=True)
+    temp3: Mapped[float] = mapped_column(nullable=True)
+    soil_temp1: Mapped[float] = mapped_column(nullable=True)
+    soil_hum1: Mapped[float] = mapped_column(nullable=True)
+    soil_temp2: Mapped[float] = mapped_column(nullable=True)
+    soil_hum2: Mapped[float] = mapped_column(nullable=True)
